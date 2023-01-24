0fc852bb2f9c1f9167091f996a313f4756fab16b
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2023-01-24 08:17:20 +0800

    Font imported on css not on component

--- layouts/MainLayout/MainLayout.tsx
+++ layouts/MainLayout/MainLayout.tsx
@@ -1,16 +1,13 @@
 import React, { FC, PropsWithChildren } from "react";
 import { Navigation } from "components/Global";
 import classes from "./mainLayout.module.scss";
-import { Content, Poppins } from "@next/font/google";
 import ContentContainer from "layouts/ContentContainer/ContentContainer";
 
-const poppins = Poppins({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
+
 
 const MainLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className={`${classes.container} ${poppins.className}`}>
+    <div className={`${classes.container}`}>
       <Navigation />
       <main>
         <ContentContainer>{children}</ContentContainer>
