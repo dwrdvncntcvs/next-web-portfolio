0e23014ebebe492ae05a49346cd357883fbc5e48
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2023-06-28 15:57:05 +0800

    Use store dispatch and state on static props

--- pages/experiences/[experienceId]/index.tsx
+++ pages/experiences/[experienceId]/index.tsx
@@ -10,86 +10,87 @@ import { customImageLoader } from "utils/helper";
 import classes from "styles/experience.module.scss";
 import { ExperienceIntro, ExperienceProjects } from "components/Experiences";
 import { HOSTNAME } from "variables";
+import { getStoreState, storeDispatch } from "store/redux";
+import { getExperiencesData } from "store/redux/api/experienceApi";
 
 interface ExperienceDetailsProps {
-  data: ExperienceData;
+    data: ExperienceData;
 }
 
 const ExperienceDetails: FC<ExperienceDetailsProps> = ({ data }) => {
-  return (
-    <>
-      <Head>
-        <title>Experience | {data.companyName}</title>
-        <meta
-          property="og:title"
-          content={`Experience | ${data.companyName}`}
-        />
-        <meta property="og:description" content={data.description} />
-        <meta property="og:image" content={`${data.companyLogo}w=256`} />
-        <meta property="og:type" content="website" />
-        <meta property="fb:app_id" content="864647734963796" />
-        <meta property="og:url" content={HOSTNAME} />
-      </Head>
-      <section className={classes.experience}>
-        <Header
-          description={`"${data.description}"`}
-          title={data.companyName}
-        />
+    return (
+        <>
+            <Head>
+                <title>Experience | {data.companyName}</title>
+                <meta
+                    property="og:title"
+                    content={`Experience | ${data.companyName}`}
+                />
+                <meta property="og:description" content={data.description} />
+                <meta
+                    property="og:image"
+                    content={`${data.companyLogo}w=256`}
+                />
+                <meta property="og:type" content="website" />
+                <meta property="fb:app_id" content="864647734963796" />
+                <meta property="og:url" content={HOSTNAME} />
+            </Head>
+            <section className={classes.experience}>
+                <Header
+                    description={`"${data.description}"`}
+                    title={data.companyName}
+                />
 
-        <div className={classes.content}>
-          <Image
-            src={data.companyLogo}
-            alt={data.companyName}
-            width={500}
-            height={500}
-            loader={customImageLoader}
-          />
-          <div className={classes.details}>
-            <ExperienceIntro
-              companyLink={data?.companyLink}
-              experienceMeta={data.meta}
-              position={data.position}
-            />
-            <ExperienceProjects projects={data?.positions} />
-          </div>
-        </div>
-      </section>
-    </>
-  );
+                <div className={classes.content}>
+                    <Image
+                        src={data.companyLogo}
+                        alt={data.companyName}
+                        width={500}
+                        height={500}
+                        loader={customImageLoader}
+                    />
+                    <div className={classes.details}>
+                        <ExperienceIntro
+                            companyLink={data?.companyLink}
+                            experienceMeta={data.meta}
+                            position={data.position}
+                        />
+                        <ExperienceProjects projects={data?.positions} />
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const experiencesRef = collection(db, "experiences");
-  const experiencesDocs = await getDocs(experiencesRef);
-  const exp_data = experiencesDocs.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  }))[0] as Experience;
+    await storeDispatch(getExperiencesData());
+    const { data } = getStoreState("experienceData");
 
-  const paths = exp_data.experiences.map(({ id }) => ({
-    params: { experienceId: id },
-  }));
+    const paths = data!.experiences.map(({ id }) => ({
+        params: { experienceId: id },
+    }));
 
-  return { paths, fallback: false };
+    return { paths, fallback: false };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { experienceId } = params as { experienceId: string };
+    const { experienceId } = params as { experienceId: string };
 
-  const docRef = doc(db, "experienceDetails", experienceId);
-  const exp_doc = await getDoc(docRef);
-  const doc_data = exp_doc.data();
+    const docRef = doc(db, "experienceDetails", experienceId);
+    const exp_doc = await getDoc(docRef);
+    const doc_data = exp_doc.data();
 
-  const data = {
-    ...doc_data,
-    meta: {
-      ...doc_data?.meta,
-      dateEnded: JSON.stringify(doc_data?.meta.dateEnded),
-      dateStarted: JSON.stringify(doc_data?.meta.dateStarted),
-    },
-  };
+    const data = {
+        ...doc_data,
+        meta: {
+            ...doc_data?.meta,
+            dateEnded: JSON.stringify(doc_data?.meta.dateEnded),
+            dateStarted: JSON.stringify(doc_data?.meta.dateStarted),
+        },
+    };
 
-  return { props: { data }, revalidate: 1 };
+    return { props: { data }, revalidate: 1 };
 };
 
 export default ExperienceDetails;
